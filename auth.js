c7345d1d65e76284a19a6d70bfd94827aeae3937
EC-GID <ecloudsm@gmail.com>
2025-07-16 12:31:59 +0800

    live

--- auth.js
+++ auth.js
@@ -16,7 +16,7 @@ app.set('trust proxy', true);
 
 const allowedOrigins = new Set([
   'https://ec360.netlify.app',
-  'https://6876bd8e509527000895cee6--ec360.netlify.app'
+  'https://68772ab806f07400081d8cbe--ec360.netlify.app'
 ]);
 
 
